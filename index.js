d4a8ed01ab677da98a4e3a940c7e9723d9cbeac5
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-02 23:42:47 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+const names = ["Violett", "Fred", "Brian"];
+let event = "surprise";
+
+
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(
+            `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        );
+    }
+    return thankYouCards;
+
+}
+
+function countDown(startInt) {
+    while (startInt > 0) {
+       console.log(startInt);
+       startInt -= 1;
+    }
+   console.log(startInt);
+} 
+
+  
